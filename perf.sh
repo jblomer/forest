e7b3f30502676337abb487fd03e505b863fe46d1
Jakob Blomer <jblomer@cern.ch>
2018-05-22 14:02:45 +0200

    add perf script

---
+++ perf.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+CMD="$1"
+OUTPUT="perf-${CMD}-$(date +"%Y%m%d-%H:%M").svg"
+
+perf record -g "./$CMD"
+perf script | /opt/FlameGraph/stackcollapse-perf.pl | /opt/FlameGraph/flamegraph.pl > "$OUTPUT"
+chromium "file://$PWD/$OUTPUT"
+
